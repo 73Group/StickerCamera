03a9632260ebce4f358abf93443bd579ea1bada0
hfutxqd <ahtlxqd@gmail.com>
2017-04-04 20:37:04 +0800

    添加录音和播放的工具类

--- app/src/main/java/com/stickercamera/AppConstants.java
+++ app/src/main/java/com/stickercamera/AppConstants.java
@@ -10,6 +10,7 @@ public class AppConstants {
     public static final String APP_DIR                    = Environment.getExternalStorageDirectory() + "/StickerCamera";
     public static final String APP_TEMP                   = APP_DIR + "/temp";
     public static final String APP_IMAGE                  = APP_DIR + "/image";
+    public static final String APP_ADUIO                  = APP_DIR + "/audio";
 
     public static final int    POST_TYPE_POI              = 1;
     public static final int    POST_TYPE_TAG              = 0;
